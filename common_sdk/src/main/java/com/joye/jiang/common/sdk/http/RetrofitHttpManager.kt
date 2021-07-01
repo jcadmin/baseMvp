52ee1b34ab380813c1a4072d9edaaa4e9702b1e1
jiangchong <492424581@qq.com>
2021-07-01 11:04:24 +0800

    优化网络多域名

--- common_sdk/src/main/java/com/joye/jiang/common/sdk/http/RetrofitHttpManager.kt
+++ common_sdk/src/main/java/com/joye/jiang/common/sdk/http/RetrofitHttpManager.kt
@@ -1,44 +1,65 @@
 package com.joye.jiang.common.sdk.http
 
-import android.text.TextUtils
 import com.lib.common.network.http.HttpConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.lang.NullPointerException
 import java.util.concurrent.TimeUnit
-
 
 class RetrofitHttpManager private constructor() {
 
-    private var mRetrofit: Retrofit? = null
+    private var mRetrofitMap: HashMap<String, Retrofit> = hashMapOf()
+
+    private val builder = OkHttpClient.Builder()
+        .connectTimeout(DEFAULT_TIME_OUT.toLong(), TimeUnit.SECONDS)
+        .readTimeout(DEFAULT_READ_TIME_OUT.toLong(), TimeUnit.SECONDS)
+        .writeTimeout(DEFAULT_WRITE_TIME_OUT.toLong(), TimeUnit.SECONDS)
+        .cookieJar(HttpConfig.mCookieJar!!)
 
     init {
-
-        val builder = OkHttpClient.Builder()
-            .connectTimeout(DEFAULT_TIME_OUT.toLong(), TimeUnit.SECONDS)
-            .readTimeout(DEFAULT_READ_TIME_OUT.toLong(), TimeUnit.SECONDS)
-            .writeTimeout(DEFAULT_WRITE_TIME_OUT.toLong(), TimeUnit.SECONDS)
-            .cookieJar(HttpConfig.mCookieJar!!)
         HttpConfig.mInterceptors?.forEach {
             builder.addInterceptor(it)
         }
+    }
 
-        if (!TextUtils.isEmpty(HttpConfig.mBaseUrl)) {
-            mRetrofit = Retrofit.Builder()
+    fun <T> create(clazz: Class<T>): T {
+        return create(HttpConfig.mBaseUrl, clazz)
+    }
+
+    fun <T> create(baseUrl: String?, clazz: Class<T>): T {
+        when {
+            //当传入的域名为空时
+            baseUrl.isNullOrEmpty() -> {
+                throw  NullPointerException()
+            }
+            //当域名已经创建时
+            mRetrofitMap.containsKey(baseUrl) -> {
+                return mRetrofitMap[baseUrl]!!.create(clazz)
+            }
+            else -> {
+                val retrofit = createRetrofit(baseUrl)
+                mRetrofitMap[baseUrl] = retrofit
+                return retrofit.create(clazz)
+            }
+        }
+    }
+
+    private fun createRetrofit(baseUrl: String?): Retrofit {
+        if (baseUrl.isNullOrEmpty()) {
+            throw  NullPointerException()
+        } else {
+            return Retrofit.Builder()
                 .client(builder.build())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(ResponseConverterFactory.create())
-                .baseUrl(HttpConfig.mBaseUrl!!)
+                .baseUrl(baseUrl)
                 .build()
         }
     }
 
     private object SingletonHolder {
         val INSTANCE = RetrofitHttpManager()
-    }
-
-    fun <T> create(clazz: Class<T>): T {
-        return mRetrofit!!.create(clazz)
     }
 
     companion object {
